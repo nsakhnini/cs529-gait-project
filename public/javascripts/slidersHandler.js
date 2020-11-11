f3dbf1ee440784526932050fd6eac0ae90195a9b
Nina Sakhnini <sakhnini.nina@gmail.com>
2020-11-10 19:16:05 -0600

    made filter panel collapsible

--- public/javascripts/slidersHandler.js
+++ public/javascripts/slidersHandler.js
@@ -16,3 +16,13 @@ function sliderUpperHandleController(val){
     children[9].style.left=value+'%';children[13].style.left=value+'%';
     children[13].childNodes[1].innerHTML=val.value;
 }
+
+function openNav() {
+    document.getElementById("filter-sidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("filter-sidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
