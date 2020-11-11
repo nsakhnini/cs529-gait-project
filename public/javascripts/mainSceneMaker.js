496a1d707982451fbe523d419cecb9e03ffc7291
Nina Sakhnini <sakhnini.nina@gmail.com>
2020-11-10 20:47:15 -0600

    fixed main view sizing when toggling sidebars

--- public/javascripts/mainSceneMaker.js
+++ public/javascripts/mainSceneMaker.js
@@ -1,8 +1,22 @@
+// Find the latest version by visiting https://unpkg.com/three. The URL will
+// redirect to the newest stable release.
+// import * as THREE from 'https://unpkg.com/three@0.121.1/build/three.module.js';
+// import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitControls.js";
+
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerWidth, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, (window.innerWidth/2)/ (window.innerHeight*0.9) , 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth*0.5,window.innerWidth*0.4 );
+renderer.setSize( (window.innerWidth/2),window.innerHeight *0.9);
+
+//Adding orbit controls to rotate , zoom and pan
+//scene.controls = new OrbitControls(camera, renderer.domElement);
+// scene.controls.mouseButtons = {
+//     LEFT: THREE.MOUSE.ROTATE,
+//     MIDDLE: THREE.MOUSE.DOLLY,
+//     RIGHT: THREE.MOUSE.PAN
+// }
+
 document.getElementById("main-scene").appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry();
@@ -13,9 +27,9 @@ scene.add( cube );
 camera.position.z = 5;
 
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerWidth;
+    camera.aspect = (window.innerWidth/2) /(window.innerHeight*0.9);
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth*0.5, window.innerWidth*0.5)
+    renderer.setSize((window.innerWidth/2),window.innerHeight*0.9);
 }
 
 window.addEventListener('resize', onWindowResize)
--- public/javascripts/sideCollapseHandler.js
+++ public/javascripts/sideCollapseHandler.js
@@ -2,18 +2,44 @@ var isFilterToggled = false;
 var is2DToggled = false;
 
 function toggleFilterBar() {
-    if(!isFilterToggled){
-        document.getElementById("filter-sidebar").style.maxWidth = window.innerWidth/9 + "px";
+    if (!isFilterToggled) {
+        document.getElementById("filter-sidebar").style.maxWidth = window.innerWidth / 12 + "px";
         document.getElementsByClassName("filter-toggle-btn")[0].innerText = ">";
         isFilterToggled = true;
-        //TODO: Resize 3D main view
+
+        //resize main scene
+        if(is2DToggled) {
+            document.getElementById("scene-section").style.maxWidth = 10 * (window.innerWidth / 12) + "px";
+            camera.aspect = 10 * (window.innerWidth / 12) / (window.innerHeight * 0.9);
+            camera.updateProjectionMatrix();
+            renderer.setSize(10 * (window.innerWidth / 12), window.innerHeight * 0.9);
+        }
+        else{
+            document.getElementById("scene-section").style.maxWidth = 8 * (window.innerWidth / 12) + "px";
+            camera.aspect = 8 * (window.innerWidth / 12) / (window.innerHeight * 0.9);
+            camera.updateProjectionMatrix();
+            renderer.setSize(8 * (window.innerWidth / 12), window.innerHeight * 0.9);
+        }
         //TODO: Remove filter bar content and short hand
-    }
-    else {
-        document.getElementById("filter-sidebar").style.maxWidth = window.innerWidth/3 + "px";
+    } else {
+        document.getElementById("filter-sidebar").style.maxWidth = window.innerWidth / 4 + "px";
         document.getElementsByClassName("filter-toggle-btn")[0].innerText = "<";
         isFilterToggled = false;
-        //TODO: Resize 3D main view
+
+        //resize main scene
+        if(is2DToggled) {
+            document.getElementById("scene-section").style.maxWidth = 8 * (window.innerWidth / 12) + "px";
+            camera.aspect = 8 * (window.innerWidth / 12) / (window.innerHeight * 0.9);
+            camera.updateProjectionMatrix();
+            renderer.setSize(8 * (window.innerWidth / 12), window.innerHeight * 0.9);
+        }
+        else{
+            document.getElementById("scene-section").style.maxWidth = 6 * (window.innerWidth / 12) + "px";
+            camera.aspect = 6 * (window.innerWidth / 12) / (window.innerHeight * 0.9);
+            camera.updateProjectionMatrix();
+            renderer.setSize(6 * (window.innerWidth / 12), window.innerHeight * 0.9);
+        }
+
         //TODO: Remove short hand and add filter bar content
     }
 
@@ -21,17 +47,41 @@ function toggleFilterBar() {
 
 function toggle2DBar() {
     if(!is2DToggled){
-        document.getElementById("two-d-sidebar").style.maxWidth = window.innerWidth/9 + "px";
+        document.getElementById("two-d-sidebar").style.maxWidth = window.innerWidth/12 + "px";
         document.getElementsByClassName("two-d-toggle-btn")[0].innerText = "<";
         is2DToggled = true;
-        //TODO: Resize 3D main view
+        //resize main scene
+        if(isFilterToggled) {
+            document.getElementById("scene-section").style.maxWidth = 10 * (window.innerWidth / 12) + "px";
+            camera.aspect = 10 * (window.innerWidth / 12) / (window.innerHeight * 0.9);
+            camera.updateProjectionMatrix();
+            renderer.setSize(10 * (window.innerWidth / 12), window.innerHeight * 0.9);
+        }
+        else{
+            document.getElementById("scene-section").style.maxWidth = 8 * (window.innerWidth / 12) + "px";
+            camera.aspect = 8 * (window.innerWidth / 12) / (window.innerHeight * 0.9);
+            camera.updateProjectionMatrix();
+            renderer.setSize(8 * (window.innerWidth / 12), window.innerHeight * 0.9);
+        }
         //TODO: Remove filter bar content and short hand
     }
     else {
-        document.getElementById("two-d-sidebar").style.maxWidth = window.innerWidth/3 + "px";
+        document.getElementById("two-d-sidebar").style.maxWidth = window.innerWidth/4 + "px";
         document.getElementsByClassName("two-d-toggle-btn")[0].innerText = ">";
         is2DToggled = false;
-        //TODO: Resize 3D main view
+        //resize main scene
+        if(isFilterToggled) {
+            document.getElementById("scene-section").style.maxWidth = 8 * (window.innerWidth / 12) + "px";
+            camera.aspect = 8 * (window.innerWidth / 12) / (window.innerHeight * 0.9);
+            camera.updateProjectionMatrix();
+            renderer.setSize(8 * (window.innerWidth / 12), window.innerHeight * 0.9);
+        }
+        else{
+            document.getElementById("scene-section").style.maxWidth = 6 * (window.innerWidth / 12) + "px";
+            camera.aspect = 6 * (window.innerWidth / 12) / (window.innerHeight * 0.9);
+            camera.updateProjectionMatrix();
+            renderer.setSize(6 * (window.innerWidth / 12), window.innerHeight * 0.9);
+        }
         //TODO: Remove short hand and add filter bar content
     }
 
