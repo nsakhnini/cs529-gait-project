2711555ba32f21ec765d24e7efb38910727ebe5f
Gino Ureta <ing.gino@gmail.com>
2020-11-12 13:58:32 -0600

    Collapse view updated

--- public/javascripts/sideCollapseHandler.js
+++ public/javascripts/sideCollapseHandler.js
@@ -23,6 +23,9 @@ function toggleFilterBar() {
             s.renderer.setSize(8 * (window.innerWidth / 12), window.innerHeight * 0.9);
         }
         //TODO: Remove filter bar content and short hand
+        renderCollapsedPanelView();
+
+
     } else {
         document.getElementById("filter-sidebar").style.maxWidth = window.innerWidth / 4 + "px";
         document.getElementsByClassName("filter-toggle-btn")[0].innerText = "<";
@@ -43,6 +46,7 @@ function toggleFilterBar() {
         }
 
         //TODO: Remove short hand and add filter bar content
+        renderExpandedPanelView();
     }
 
 }
@@ -87,6 +91,35 @@ function toggle2DBar() {
         //TODO: Remove short hand and add filter bar content
     }
 
+}
+function renderCollapsedPanelView(){
+    document.getElementById("participant-list").style.display = "none";
+    document.getElementById("slider-age").style.display = "none";
+    document.getElementById("slider-height").style.display = "none";
+    document.getElementById("slider-weight").style.display = "none";
+    document.getElementById("gender-form").style.display = "none";
+    document.getElementById("filter-btn").style.visibility = "hidden";
+
+    document.getElementById("participant-list-readyonly").style.display = "block";
+    document.getElementById("age-viewonly").style.display = "block";
+    document.getElementById("height-readonly").style.display = "block";
+    document.getElementById("weight-readonly").style.display = "block";
+    document.getElementById("gender-readonly").style.display = "block";
+    
+}
+function renderExpandedPanelView(){
+    document.getElementById("participant-list").style.display = "block";
+    document.getElementById("slider-age").style.display = "block";
+    document.getElementById("slider-height").style.display = "block";
+    document.getElementById("slider-weight").style.display = "block";
+    document.getElementById("gender-form").style.display = "block";
+    document.getElementById("filter-btn").style.visibility = "visible";
+
+    document.getElementById("participant-list-readyonly").style.display = "none";
+    document.getElementById("age-viewonly").style.display = "none";
+    document.getElementById("height-readonly").style.display = "none";
+    document.getElementById("weight-readonly").style.display = "none";
+    document.getElementById("gender-readonly").style.display = "none";
 }
 
 document.querySelector("#minimize-filter").addEventListener('click', toggleFilterBar);
